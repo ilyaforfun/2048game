8ef4c4942a22d1245ea494a6f22fddc5e1159f82
ilyaforfun <146834843+ilyaforfun@users.noreply.github.com>
2024-07-24 08:57:41 +1000

    Upd2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Startup2048 from './Startup2048';
-import './index.css'; 
+import './index.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Exit Your Startup</title>
+      </Helmet>
       <Startup2048 />
     </div>
   );
